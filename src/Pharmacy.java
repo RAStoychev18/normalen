258367ff17c3371355f69e0bf2d10bd07e3194ce
RAStoychev18 <RAStoychev18@codingburgas.bg>
2022-12-13 15:35:32 +0200

    initial

--- src/Pharmacy.java
+++ src/Pharmacy.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Pharmacy {
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Pharmacy {
+
+    public String name;
+    public double price;
+    public int quantity;
+    public LocalDate expirationDate;
+
+    public Pharmacy(String name, double price, int quantity, LocalDate expirationDate) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.expirationDate = expirationDate;
+    }
+
+    public Pharmacy() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Pharmacy{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", expirationDate='" + expirationDate + '\'' +
+                '}';
+    }
 }
